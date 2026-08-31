7340673db44fdc4b3d7bda445160a78f3846f8cc
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-08-31 14:18:44 -0400

    homework 1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//            double result = 8.0 / 3.0;
+//        int m = 2;
+//        int n = 3;
+//        int p = 4;
+//        int q = 5;
+//        double r = 6;
+//
+//        System.out.println(m + n);
+//        System.out.println(p * q);
+//        System.out.println(m + n + r);
+//        System.out.println(p * q * m);
+//        System.out.println(r - m);
+
+//        double result = 7.0 / 2 * 3 % 2;
+        int k = 3;
+        int j = 4;
+        int m  = 5;
+
+        m *= ++j + ++k;
+
+        System.out.println(k);
+        System.out.println(j);
+        System.out.println(m);
+        }
+    }
