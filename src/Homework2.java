39933850f499036f37a14095bb8df3ce2e798151
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-08-31 14:56:34 -0400

    Homework 2

---
+++ src/Homework2.java
@@ -0,0 +1,46 @@
+public class Homework2 {
+
+    public static void main(String[] args) {
+//        int k = 5;
+//        int i = -2;
+//
+//        while ( i <= k ) {
+//            i = i + 2;
+//            k = k - 1;
+//            System.out.println(i+k);
+//        }
+
+//        int i = -2;
+//        while (i <= 2) {
+//            i = i * i * i - 3 * i + 1;
+//            System.out.println(i);
+//        }
+
+//        for (int i = 1; i <= 2; i++) {
+//            for (int j =1; j <= 2; j++) {
+//                for (int k = 1; k <= j; k++) {
+//                    System.out.println(i + " " + j + " " + k);
+//                }
+//            }
+//        }
+//        for (int i = 1; i <= 2; i++) {
+//            for (int j =1; j <= 2; j++) {
+//                for (int k = 1; k <= i; k++) {
+//                    System.out.println(i + " " + j + " " + k);
+//                }
+//            }
+//        }
+
+//        int k = 5;
+//        for (int i = 1; i <= k; i++) {
+//            System.out.println(i+k);
+//            k = 2;
+//        }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            i = i - 1;
+        }
+
+    }
+}
