b3f55afe4476f715bcfce1705f28b0421b7d759c
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-09-10 10:28:19 -0400

    Stack (main) in java

---
+++ src/Lab9_10.java
@@ -0,0 +1,17 @@
+public class Lab9_10 {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
+}
