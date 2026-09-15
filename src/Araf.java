7927a9f7f12d7d7d2adf44954d84e5da7ddeaee5
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-09-15 10:21:46 -0400

    DSA: stacks example

--- src/Araf.java
+++ src/Araf.java
@@ -1,132 +1,144 @@
-import java.util.Random;
-import java.util.Scanner;
-
-public class Araf {
-    public static void main(String[] args)
-    {
-        Scanner scanner = new Scanner(System.in);
-
-        //for loop to create 16 cards
-        int[] nums = new int[16];
-        for (int i = 0; i < 8; i++)
-        {
-            //two copies of array from 1-8
-            nums[i] = i + 1;
-            nums[i + 8] = i + 1;
-        }
-
-        //using random to shuffle
-        Random rand = new Random();
-        for (int i = nums.length - 1; i > 0; i--)
-        {
-            int j = rand.nextInt(i + 1);
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
-
-        //put numbers in 4x4 grid
-        int[][] grid = new int[4][4];
-        boolean[][] revealed = new boolean[4][4];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
-            {
-                grid[i][j] = nums[i * 4 + j];
-                revealed[i][j] = false;
-            }
-        }
-
-        //this is to handle pairs solved
-        int pairsFound = 0;
-
-        //loop to run until all 8 pairs are found
-        while (pairsFound < 8)
-        {
-            //print board
-            System.out.println("   1 2 3 4");
-            for (int i = 0; i < 4; i++)
-            {
-                System.out.print((i + 1) + "  ");
-                for (int j = 0; j < 4; j++)
-                {
-                    //show number if revealed)
-                    if (revealed[i][j]) {
-                        System.out.print(grid[i][j] + " ");
-                    }
-                    else
-                    {
-                        //show * if hidden
-                        System.out.print("* ");
-                    }
-                }
-                System.out.println();
-            }
-
-            //ask for first guess
-            System.out.println("Enter row and column of first guess (1-4 each): ");
-            int row1 = scanner.nextInt() - 1;
-            int col1 = scanner.nextInt() - 1;
-
-            //check if guess is correct
-            if (row1 < 0 || row1 >= 4 || col1 < 0 || col1 >= 4 || revealed[row1][col1])
-            {
-                System.out.println("Invalid guess, please try again!");
-                continue;
-            }
-
-            //show the first guess
-            System.out.println("you picked: " + grid[row1][col1]);
-
-            //ask for second guess
-            System.out.println("Enter row and column of second guess (1-4 each): ");
-            int row2 = scanner.nextInt() - 1;
-            int col2 = scanner.nextInt() - 1;
-
-            //check if second guess is correct
-            if (row2 < 0 || row2 >= 4 || col2 < 0 || col2 >= 4 || revealed[row2][col2])
-            {
-                System.out.println("Invalid guess, please try again!");
-                continue;
-            }
-
-            //handling the case of picking same card
-            if (row1 == row2 && col1 == col2)
-            {
-                System.out.println("you picked the same card twice!");
-                continue;
-            }
-
-            //shoe second guess
-            System.out.println("you picked: " + grid[row2][col2]);
-
-            //check if two guesses matched
-            if (grid[row1][col1] == grid[row2][col2])
-            {
-                System.out.println("Match Found! Lessgo!");
-                revealed[row1][col1] = true;
-                revealed[row2][col2] = true;
-                pairsFound++;
-            }
-            else
-            {
-                System.out.println("Boo, not a match, press Enter to continue");
-                scanner.nextLine(); // clear leftover newline
-                scanner.nextLine(); // wait for Enter
-            }
-        }
-
-        // Final board
-        System.out.println("   1 2 3 4");
-        for (int i = 0; i < 4; i++)
-        {
-            System.out.print((i + 1) + "  ");
-            for (int j = 0; j < 4; j++)
-            {
-                System.out.print(grid[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("Congratulations! You win!");
-    }
-}
-
+//import java.util.Scanner;
+//
+//public class Account1 {
+//    private String name;
+//    private double balance;
+//
+//    public Account1 (String argName, double argBalance) {
+//        name = argName;
+//        balance = argBalance;
+//    }
+//
+//    //    Method to print the balance of the user
+//    public void showBalance() {
+//        System.out.printf("%s: Your balance = $%.2f%n", name, balance);
+//    }
+//
+//    //    Getter method to help access the user's name.
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public double getBalance() {
+//        return balance;
+//    }
+//
+//    public void deposit(double amount) {
+//
+//        balance = balance + amount;
+//    }
+//
+//    //    Method to help on withdraw function
+//    public boolean withdraw(double amount) {
+//        if (amount > balance) {
+//            return false;
+//        }
+//
+//        balance = balance - amount;
+//        return true;
+//    }
+//
+//    //    toString method to help print balance message
+//    public String toString() {
+//        return String.format("%s: Your balance = $%.2f", name, balance);
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (!(o instanceof Account other)) {
+//            return false;
+//        }
+//        return name.equals(other.name) && this.balance == other.balance;
+//    }
+//
+//}
+//
+//public class Bank {
+//    private Account[] accounts;
+//    private int count;
+//
+//    public Bank(int total) {
+//        accounts = new Account[total];
+//        count = 0;
+//    }
+//
+//    //    find method to check if there is an account with a particular name
+//    public Account find (String name) {
+//        for (int i = 0; i < count; i++) {
+//            if (accounts[i].getName().equals(name)) {
+//                return accounts[i];
+//            }
+//        }
+//
+//        return null;
+//    }
+//
+//    //    Add an account to the array
+//    public void addAccount(Account a) {
+//        if (count < accounts.length) {
+//            accounts[count] = a;
+//            count++;
+//        }
+//        else {
+//            System.out.println("Bank is full - cannot add account.");
+//        }
+//    }
+//    //    Method to print the total deposits in a session
+//    public void printTotal() {
+//        double total = 0.0;
+//        for (int i = 0; i < count; i++) {
+//            total = total + accounts[i].getBalance();
+//        }
+//        System.out.printf("All deposits in the bank: $%.2f%n", total);
+//    }
+//
+//}
+//
+//public class Araf {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        Bank bank = new Bank(10);
+//
+//        System.out.println("Enter your name please: ");
+//        String name = input.nextLine();
+//
+////        condition to check if there is similar account
+//        Account account = bank.find(name);
+//        if (account == null) {
+//            account = new Account(name, 100.00);
+//            bank.addAccount(account);
+//        }
+//
+//        int choice = 0;
+////        main while loop
+//        while (choice != 4) {
+//            System.out.println("1. Deposit");
+//            System.out.println("2. Withdraw");
+//            System.out.println("3. Show my balance");
+//            System.out.println("4. Exit");
+//            System.out.println("Enter your choice please: ");
+//
+//            choice = input.nextInt();
+//
+//            if (choice == 1) {
+//                System.out.println("Enter the amount: ");
+//                double amount = input.nextDouble();
+//                account.deposit(amount);
+//            }
+//            else if (choice == 2) {
+//                System.out.println("Enter the amount: ");
+//                double amount = input.nextDouble();
+//                if (!account.withdraw(amount)){
+//                    System.out.println("Not enough money for the withdrawal.");
+//                }
+//            }
+//            else if (choice == 3) {
+//                account.showBalance();
+//            } else if (choice == 4) {
+//                bank.printTotal();
+//            }
+//        }
+//        input.close();
+//    }
+//}
--- src/Lab9_10.java
+++ src/Lab9_10.java
@@ -1,12 +1,29 @@
 public class Lab9_10 {
 
+    <T> void printStack(Stack<T> s) {
+        Stack<T> t = new Stack<>();
+        while (!s.isEmpty()) {
+            t.push(s.pop());
+            System.out.print(t.topEl() + " ");
+        }
+        System.out.println();
+        while (!t.isEmpty()) {
+            s.push(t.pop());
+        }
+    }
+
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
+        Lab9_10 lab = new Lab9_10();
+
         stack.push(10);
         stack.push(20);
         stack.push(30);
 
         System.out.println(stack);
+
+        lab.printStack(stack);
+
         System.out.println(stack.pop());
         System.out.println(stack);
         System.out.println(stack.pop());
